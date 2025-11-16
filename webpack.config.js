afbaeb42f7e13ca69958c871fd031f0f873be677
waleedtaha3e <waleedtaha3e@gmail.com>
2025-11-16 20:13:47 +0400

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    taskpane: "./src/taskpane.ts"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    },
+    port: 3000,
+    hot: true,
+    https: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
+};
 
